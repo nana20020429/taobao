0f3c183e884e0ac800c7e0cfdd04527908ea997e
nana20020429 <105772200+nana20020429@users.noreply.github.com>
2022-06-29 12:15:26 +0800

    Add files via upload

---
+++ taobao/taobao/src/api/base.js
@@ -0,0 +1,10 @@
+/**
+ * 接口域名的管理
+ */
+const base = {
+  // sq: 'https://localhost:44369',  //本地开发时的测试地址
+   sq: "http://www.hj0819.top:44369", //阿里云服务器 ，发布地址
+  //sq:"http://www.oct1001.icu", // https证书有问题 取消当前接口
+  ip: "http://pv.sohu.com",
+};
+export default base;
